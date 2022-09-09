8a816fc7420e2e593f2f4fe46fffe22f0516aeb1
Lucca-Sabbatini-IdUFF <luccasabbatini@id.uff.br>
2022-09-09 10:44:07 -0300

    feat: ball intial collision logic

--- game.py
+++ game.py
@@ -1,14 +1,34 @@
+# Imports
 from PPlay.sprite import *
 from PPlay.window import *
 
+# Game Window Initialization
 gameWindow = Window(1200,800)
 gameWindow.set_title("Pong")
-gameWindow.set_background_color ((0,0,0))
 
+# Ball Initialization
 ball = Sprite("./assets/images/ball.png", 1)
 ball.x = (gameWindow.width / 2) - (ball.width / 2)
 ball.y = (gameWindow.height / 2) - (ball.height / 2)
 
+# Ball Speed
+ballSpeedX = 1
+ballSpeedY = 1
+
+# Game Loop
 while (gameWindow):
+    gameWindow.clear()
+    gameWindow.set_background_color((0, 0, 0))
+
+    ball.x += ballSpeedX
+    ball.y += ballSpeedY
+
+    if (((ball.x + ball.width) == gameWindow.width) or (ball.x == 0)):
+        ballSpeedX = ballSpeedX * -1
+
+    if (((ball.y + ball.height) == gameWindow.height) or (ball.y == 0)):
+        ballSpeedY = ballSpeedY * -1
+
     ball.draw()
+
     gameWindow.update()
